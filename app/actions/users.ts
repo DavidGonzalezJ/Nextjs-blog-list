81bc0fe62433704ead091ebbebcc2d3a144bb746
David <davidgonzalezj97@gmail.com>
2026-06-28 00:17:39 +0200

    Fixed: new token now can't be the same as previous one

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -1,7 +1,6 @@
 "use server"
 
 import { redirect } from "next/navigation"
-import { revalidatePath } from "next/cache"
 import bcrypt from "bcryptjs"
 import { eq } from "drizzle-orm"
 import { db } from "@/db"
@@ -52,5 +51,5 @@ export const generateToken = async () => {
   const token = crypto.randomUUID()
   await db.update(users).set({ token }).where(eq(users.id, user.id))
 
-  revalidatePath("/me")
+  redirect("/me")
 }
